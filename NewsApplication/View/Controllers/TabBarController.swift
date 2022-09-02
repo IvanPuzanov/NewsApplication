71fa0e5b6d67a5823ea3a2d746124e718c17bb5a
Ivan Puzanov <ivanpuzan0v.dev@gmail.com>
2022-09-02 12:44:34 +0300

    Commit

---
+++ NewsApplication/View/Controllers/TabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarController.swift
+//  NewsApplication
+//
+//  Created by Ivan Puzanov on 20.08.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    private let newsCoordinator = NewsCoordinator(UINavigationController())
+    private let profileCoordinator = ProfileCoordinator(UINavigationController())
+    
+    // MARK: -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        newsCoordinator.start()
+        profileCoordinator.start()
+        
+        UITabBar.appearance().tintColor = UIColor(named: "tabBarSelected")
+        
+        viewControllers = [newsCoordinator.navigationController, profileCoordinator.navigationController]
+    }
+
+}
